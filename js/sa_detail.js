579b7b555651cd91d33e12e7eb3c5b2636b05796
frank <18380462011@163.com>
2019-03-29 22:56:16 +0800

    详情页

--- js/sa_detail.js
+++ js/sa_detail.js
@@ -10,15 +10,6 @@ $(function(){
 	// 初始化
 	function sa_detail_init(){
 		var id = GetQueryString("id");
-		if(id==1 || id=="1"){
-			//为什么留学天津
-		}else if(id==2 || id=="2"){
-			//留学生活
-		}else if(id==3 || id=="3"){
-			//校友活动
-		}else if(id==4 || id=="4"){
-			//体验天津
-		}
 		// 获取消息详情
 		$.ajax({
 			url: 'json/sa_detail.json',
@@ -28,10 +19,32 @@ $(function(){
 			},
 			dataType: 'json',
 			success: function(resp){
-				if(resp.data && resp.data.length>0){
-					console.log(resp.data);
-					// 把详情页内容填充
-					$("#sa_detail").empty().html(resp.data);
+				if(resp.data){
+					//类别
+					var sa_typee = "为什么留学天津";
+					var sa_bg = "bg-blue";
+					if(resp.data.type==1 || resp.data.type=="1"){
+						sa_typee = "为什么留学天津";
+						sa_bg = "bg-blue";
+					}else if(resp.data.type==2 || resp.data.type=="2"){
+						sa_typee = "留学生活";
+						sa_bg = "bg-orange";
+					}else if(resp.data.type==3 || resp.data.type=="3"){
+						sa_typee = "校友活动";
+						sa_bg = "bg-green";
+					}else if(resp.data.type==4 || resp.data.type=="4"){
+						sa_typee = "体验天津";
+						sa_bg = "bg-yellow";
+					}
+					$("#sa_typee").html(sa_typee);
+					$("#sa_typee").addClass(sa_bg);
+					$("#sa_line").addClass(sa_bg);
+					//标题
+					$("#sa_detail_title").html(resp.data.title);
+					//日期
+					$("#sa_detail_date").html(resp.data.date);
+					//内容
+					$("#sa_detail_content").html(resp.data.content);
 				}
 			},
 			error: function(err){
